e0af2b1a9953ff2f6d040afc0f2bbcece12ae014
kishorep-08 <kishore.pkdp@gmail.com>
2025-10-16 12:17:36 +0530

    terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "first_resource" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = var.environment == "dev"? "t3.micro": "t3.medium"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+   
+
+  tags = {
+    Name = "terraform"
+    Terraform = "true"
+    Language = "HCL"
+  }
+
+}
+
+resource "aws_security_group" "allow_all" {
+  name   = "my_sg"
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "my_sg"
+    }
+}
