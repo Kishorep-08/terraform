239c87dd58726203d67fdb8a11c722776a2848de
kishorep-08 <kishore.pkdp@gmail.com>
2025-12-17 18:44:16 +0530

    terraform

--- devops-ec2/user_data.sh
+++ devops-ec2/user_data.sh
@@ -3,6 +3,8 @@ growpart /dev/nvme0n1 4
 pvresize /dev/nvme0n1p4
 lvextend -L 20G /dev/mapper/RootVG-varVol
 xfs_growfs /var
+
+# Install Docker
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -10,3 +12,18 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 newgrp docker
+
+# Install kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+
+# Install eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
